deda5137aa24d063150c6cdbe119810b06097f72
Mateusz Kamiński <mkaminski.contact@gmail.com>
2025-02-06 11:39:11 +0100

    styled tabs screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,34 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: 'gold',
+        tabBarActiveBackgroundColor: 'purple',
+        tabBarInactiveTintColor: 'lightgray',
+        tabBarInactiveBackgroundColor: 'purple',
+      }}
+    >
       <Tabs.Screen
         name='index'
         options={{
           title: 'Recent',
           headerTitle: 'Recent Expenses',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='hourglass' size={size} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name='allExpenses'
         options={{
           title: 'All Expenses',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='calendar' size={size} color={color} />
+          ),
         }}
       />
     </Tabs>
