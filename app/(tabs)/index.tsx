c71e0c3f96d63f9684877ee32dcaeeb7520f515b
Mateusz Kamiński <mkaminski.contact@gmail.com>
2025-02-12 16:23:28 +0100

    recent screen basic styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        padding: 14,
+        alignItems: 'center',
+        backgroundColor: 'rgb(60, 17, 109)',
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
