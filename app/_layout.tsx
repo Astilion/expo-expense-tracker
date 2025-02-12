969262ae85b3ea4f1ff1911f00fdf61e0d616590
Mateusz Kamiński <mkaminski.contact@gmail.com>
2025-02-12 16:19:00 +0100

    styled status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <StatusBar style='light' backgroundColor='rgb(66, 8, 133)' />
+      <Stack>
+        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 }
