05d52ed0e6ad15e7718f815f6d84af882c834672
Mateusz Kamiński <mkaminski.contact@gmail.com>
2025-02-13 21:23:46 +0100

    added dummy expense constant

--- components/Expense.tsx
+++ components/Expense.tsx
@@ -1,15 +1,27 @@
 import { Text, View, StyleSheet } from 'react-native';
 
 export default function Expense() {
+  //dummy expense
+  const expense = {
+    title: 'A book',
+    amount: 19.99,
+    date: new Date('2024-02-13'),
+  };
   return (
     <>
       <View style={styles.rootContainer}>
         <View style={styles.textContainer}>
-          <Text style={styles.expenseName}>A book</Text>
-          <Text style={styles.expenseDate}>2025.02.12</Text>
+          <Text style={styles.expenseName}>{expense.title}</Text>
+          <Text style={styles.expenseDate}>
+            {expense.date.toLocaleDateString('en-US', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            })}
+          </Text>
         </View>
         <View style={styles.ammountContainer}>
-          <Text style={styles.price}>19.99</Text>
+          <Text style={styles.price}>{expense.amount.toFixed(2)}</Text>
         </View>
       </View>
     </>
