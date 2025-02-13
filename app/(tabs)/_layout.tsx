193902e8d0e97cc118fbee09d9c2b351028f3c6a
Mateusz Kamiński <mkaminski.contact@gmail.com>
2025-02-13 21:31:27 +0100

    added pressable for adding new expense

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,28 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { Pressable } from 'react-native';
 
 export default function TabLayout() {
+  const addExpenseHandler = () => {
+    console.log('Add new expense');
+  };
   const tabScreenOptions = {
     headerStyle: { backgroundColor: 'rgb(66, 8, 133)' },
     headerTintColor: 'white',
     tabBarActiveTintColor: 'rgb(194, 172, 48)',
     tabBarActiveBackgroundColor: 'rgb(66, 8, 133)',
     tabBarInactiveBackgroundColor: 'rgb(66, 8, 133)',
+    headerRight: () => (
+      <Pressable
+        onPress={addExpenseHandler}
+        style={({ pressed }) => [
+          { padding: 8, marginRight: 16, opacity: pressed ? 0.7 : 1 },
+        ]}
+      >
+        <Ionicons name='add' size={24} color='white' />
+      </Pressable>
+    ),
   };
   return (
     <Tabs screenOptions={tabScreenOptions}>
